06740b86583c1431dd4d53e9343f28d443f0058e
Jacqueline S. Homan <jacquelinehoman7@gmail.com>
2013-05-25 22:41:48 -0400

    initoal commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sca::Application.config.secret_token = 'efcab8723c3fe2013681525484da3f1a9c628d1ece50c7c35b1af0cf36b9a2d247745370f065441df5dddf9a3ff617f49ad72e351b14feaf68d7081223b4536b'
