318dd942426edbdc0a1495cecd21bd3011268250
Jacqueline S. Homan <jacquelinehoman7@gmail.com>
2013-11-07 19:44:55 -0500

    modified omniauth.rb file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,9 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 	#provider :developer unless Rails.env.production?
-	provider :twitter, ENV['SCA_KEY'], ENV['SCA_SECRET']
+	provider :twitter, ENV['SCA_KEY'], ENV['SCA_SECRET'] 
 end
+# This app has to authenticate itself to Twitter, 
+# and it does that through these unique environment variables. 
+# This is the part of the app that initializes Omniauth so that Omniauth is available
+# and allows Twitter to interact with this app. 
